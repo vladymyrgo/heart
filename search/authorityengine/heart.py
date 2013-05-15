37f5145c02cdb197b60d616ca4e0bd099a35cb3d
Vladymyr Gosha <vladymyrgo@hotmail.com>
2013-05-15 18:29:34 +0300

    второй коммит

--- search/authorityengine/heart.py
+++ search/authorityengine/heart.py
@@ -52,29 +52,95 @@ class HTMLcollector(object):
                 all_links.extend(self.links_scanner(html))
                 return all_links[0:max_links]
     
-    def html_list(self, max_html=50, max_search_page=5):
+    def links_html_dict(self, max_html=50, max_search_page=5):
         '''
-        Возвращает список html-ей найденых поиском сайтов
+        Возвращает словарь из ссылок и их html-ей
         Может принимать ограничение количества возвращаемых html-ей и
         ограничение количества сканируемых страниц в поиске.
         '''
         links = self.get_links(max_search_page=max_search_page, max_links=max_html)
-        l = [urllib.urlopen(link).read() for link in links]
+        l = [[link, urllib.urlopen(link).read()] for link in links]
+        l=dict(l)
         return l
     
 class AuthorityAnalyzer(HTMLcollector):
     def extract_text(self, html):
+        '''
+        Принимает html и возвращает текст страницы
+        '''
         soup = BeautifulSoup(html)
-        text=soup.get_text()
+        #удаляем теги "style" и "script" и их содержимое
+        for_del = soup("style")
+        for_del.extend(soup("script"))
+        [tag.decompose() for tag in for_del]
+        #извлекаем текст из html
+        text = soup.get_text()
         return text
+    
+    def number_of_words(self, voc):
+        '''
+        Возвращает количество слов в статье
+        '''
+        n = sum(voc.values())
+        return n
+    
+    def vocabulary(self, text):
+        '''
+        Принимает текст и возвращает словарь из слов и количества их повторений 
+        '''
+        words = {}
+        garb = '\t\n\x0b\x0c\r !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~0123456789"\''
+        for word in text.lower().split():
+            word = word.strip(garb)
+            if len(word) > 2:
+                words[word] = words.get(word, 0) + 1
+        
+        return words
+    
+    def terms(self, voc):
+        '''
+        Возвращает количество терминов в статье
+        или сами термины (это лучше)
+        '''
+        pass
+    
+    def names(self, voc):
+        '''
+        Возвращает количество имен в статье
+        '''
+        pass
+    
+    def ranks(self, voc):
+        '''
+        Возвращает количество званий/рангов в статье
+        '''
+        pass
+    
+    def places(self, voc):
+        '''
+        Возвращает количество мест в статье
+        '''
+        pass
+    
+    def time(self, voc):
+        '''
+        Возвращает количество упоминаний времени в статье
+        '''
+        pass
+    
+    def statistics(self, text):
+        '''
+        Возвращает количество символов "%" в статье
+        '''
+        perc = text.count('%')
+        return perc
 
 if __name__ == '__main__':
     def create_file_html():
         a=HTMLcollector('храм')
         f=open('/Users/vladymyr/Desktop/htmlsites.txt','w')
-        for i in a.html_list(max_html=3):
+        for i in a.links_html_dict(max_html=3):
             f.write(i)
         f.close()
         print 'done'
-    
-    
+    create_file_html()
